aae740c0972162ab62f5af0d590f0495a9b10016
amy <amarantakruger@gmail.com>
2024-10-08 22:36:21 +0200

    ok modules

---
+++ node-js-terminal/index.js
@@ -0,0 +1,6 @@
+const {generateRandomNumber,celciusToFarenheit } = require('./utils'); // Importing the function from utils.js
+
+// Generating and logging a random number
+console.log(`Random Number: ${generateRandomNumber()}`);
+
+console.log(`Ceslcius to farenheit: ${celciusToFarenheit(0)}`);
---
+++ node-js-terminal/post.Controller.js
@@ -0,0 +1,9 @@
+const posts = [
+
+    {id: 1, title: 'Post One'},
+    {id: 1, title: 'Post Two'},
+];
+
+//
+export const getPosts = () => posts;
+
